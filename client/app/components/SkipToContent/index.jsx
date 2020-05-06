99e4e37b7caf594feaaf916930a53516ece83bc6
discombobulateme <pcultural@gmail.com>
2020-05-06 16:10:47 +0200

    add event listener

--- client/app/components/SkipToContent/index.jsx
+++ client/app/components/SkipToContent/index.jsx
@@ -1,6 +1,5 @@
 // @flow
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 import css from './SkipToContent.scss';
 import { I18n } from '../../libs/i18n';
@@ -12,10 +11,13 @@ export type Props = {
 function SkipToContent(props: Props) {
   const { id } = props;
 
+  window.addEventListener('Props', () => {
+    window.scrollTo(0, 0)});
+
   return (
-    <Link className={css.skipToContent} to={`#${id}`}>
+    <a className={css.skipToContent} href={`#${id}`}>
       {I18n.t('navigation.skip_to_main_content')}
-    </Link>
+    </a>
   );
 }
 
